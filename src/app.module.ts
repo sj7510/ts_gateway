a7b7302a409c1635ba31b3565b7cc875bfe9fb1f
huajibin <hujiabin@thunisoft.com>
2025-05-27 15:46:04 +0800

    feat: 读取环境配置相关信息。

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatewayController } from './gateway/gateway.controller';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { getConfig } from './utils';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 
 /**
  * 根模块用于处理其他类的引用与共享。
  */
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      ignoreEnvFile: true,
+      isGlobal: true,
+      load: [getConfig],
+    }),
     ClientsModule.register([
       {
         name: 'MICROSERVICE_CLIENT',

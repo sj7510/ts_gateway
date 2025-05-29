3e45b4d8fb74486161164fac06d3c3ff0dfc3fd2
huajibin <hujiabin@thunisoft.com>
2025-05-29 09:34:32 +0800

    feat: 增加mongodb对接

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,9 @@
 import { Module } from '@nestjs/common';
-import { CacheModule } from '@nestjs/cache-manager';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { GatewayController } from './gateway/gateway.controller';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { getConfig } from './utils';
-import { ClientsModule, Transport } from '@nestjs/microservices';
-import { FeishuController } from '@/user/feishu/feishu.controller';
-import { FeishuService } from '@/user/feishu/feishu.service';
+import { CacheModule } from '@nestjs/cache-manager';
 
-/**
- * 根模块用于处理其他类的引用与共享。
- */
 @Module({
   imports: [
     CacheModule.register({
@@ -23,19 +14,9 @@ import { FeishuService } from '@/user/feishu/feishu.service';
       isGlobal: true,
       load: [getConfig],
     }),
-    ClientsModule.register([
-      {
-        name: 'MICROSERVICE_CLIENT',
-        transport: Transport.TCP,
-        options: {
-          host: '0.0.0.0',
-          port: 3001,
-        },
-      },
-    ]),
     UserModule,
   ],
-  controllers: [AppController, GatewayController, FeishuController],
-  providers: [AppService, FeishuService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

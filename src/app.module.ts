4f468fcee5a18868eac6b5e6b526954fbea5a27f
huajibin <hujiabin@thunisoft.com>
2025-05-29 13:36:47 +0800

    feat: 集成Redis。

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,17 @@ import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { getConfig } from './utils';
 import { CacheModule } from '@nestjs/cache-manager';
+import * as redisStore from 'cache-manager-redis-store';
 
 @Module({
   imports: [
     CacheModule.register({
       isGlobal: true,
+      store: redisStore,
+      host: getConfig('REDIS_CONFIG').host,
+      port: getConfig('REDIS_CONFIG').port,
+      auth_pass: getConfig('REDIS_CONFIG').auth,
+      db: getConfig('REDIS_CONFIG').db,
     }),
     ConfigModule.forRoot({
       ignoreEnvFile: true,
